d96bae2fd372d5c889928a1f5be397adf93ac330
Tarik Alaiwi <tarik.alaiwi.dev@gmail.com>
2024-04-29 20:29:38 +0200

    initial

---
+++ scripts.js
@@ -0,0 +1,25 @@
+    // Po kliknięciu przycisku, przewiń stronę na górę z płynną animacją
+    $(document).ready(function() {
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 100) {
+                $('#backToTopButton').fadeIn();
+            } else {
+                $('#backToTopButton').fadeOut();
+            }
+        });
+
+        $('#backToTopButton').click(function() {
+            $('html, body').animate({scrollTop: 0}, 800); // Przewijanie do góry z animacją trwającą 800 milisekund
+            return false;
+        });
+    });
+
+    function scrollToSection(sectionNumber) {
+        var sectionId = "section" + sectionNumber;
+        var section = document.getElementById(sectionId);
+        var sectionTop = section.offsetTop;
+        window.scrollTo({
+            top: sectionTop,
+            behavior: 'smooth'
+        });
+    }
